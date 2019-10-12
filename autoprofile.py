c3e42120fd6c586041010e9aaa0f833bb8dd6d3a
pqhaa <usaguy1342@gmail.com>
2019-10-12 18:56:14 +0000

    autoprofile: Add autopfp
     - Also fix some strings in other commands
     - Help needs testing
    
    Signed-off-by: pqhaa <usaguy1342@gmail.com>

--- autoprofile.py
+++ autoprofile.py
@@ -17,6 +17,8 @@
 import asyncio
 import time
 import logging
+from PIL import Image
+from io import BytesIO
 from telethon.tl import functions
 from .. import loader, utils
 
@@ -33,15 +35,91 @@ class AutoProfileMod(loader.Module):
         self.name = _("Automatic Profile")
         self.bio_enabled = False
         self.name_enabled = False
+        self.pfp_enabled = False
+        self.pfp_remove_latest = None
         self.raw_bio = None
         self.raw_name = None
+        self.pfp_degree = 0
 
     async def client_ready(self, client, db):
         self.client = client
 
+    async def autopfpcmd(self, message):
+        """Rotates your profile picture every n seconds with x degrees, usage:
+           (prefix)autopfp <timeout> <degrees> <remove previous (last pfp)>
+
+           Timeout - seconds
+           Degrees - 60, -10, etc
+           Remove last pfp - true or false, boolean, case insensitive"""
+
+        msg = utils.get_args(message)
+        if len(msg) != 3:
+            return await message.edit(_("<b>Autopfp requires three args. See the help for syntax.</b>"))
+
+        try:
+            timeout_autopfp = int(msg[0])
+        except ValueError as e:
+            logger.warning(str(e))
+            return await message.edit(_("<b>Wrong time.</b>"))
+
+        try:
+            degrees = int(msg[1])
+        except ValueError as e:
+            logger.warning(str(e))
+            return await message.edit(_("<b>Wrong degrees value.</b>"))
+
+        if msg[2].lower() == 'true':
+            delete_previous = True
+        elif msg[2].lower() == 'false':
+            delete_previous = False
+        else:
+            return await message.edit(_("<b>Please pass true or false for previous pfp removing.</b>"))
+
+        client = self.client
+        me = await client.get_me()
+        pfp = await client.download_profile_photo(me, file=bytes)
+        raw_pfp = Image.open(BytesIO(pfp))
+
+        self.pfp_remove_latest = delete_previous
+        self.pfp_enabled = True
+        await message.edit("<b>Successfully enabled autopfp.</b>")
+
+        while self.pfp_enabled:
+            self.pfp_degree += degrees
+            rotated = raw_pfp.rotate(self.pfp_degree)
+            buf = BytesIO()
+            rotated.save(buf, format='JPEG')
+            bytes_img = buf.getvalue()
+
+            if self.pfp_remove_latest:
+                await client(functions.photos.DeletePhotosRequest(
+                    await client.get_profile_photos(await client.get_me(), limit=1)
+                ))
+
+            await client(functions.photos.UploadProfilePhotoRequest(
+                await client.upload_file(bytes_img)
+            ))
+            await asyncio.sleep(timeout_autopfp)
+
+    async def stopautopfpcmd(self, message):
+        """ Stop autobio cmd."""
+
+        if self.pfp_enabled is False:
+            return await message.edit(_("<b>Autopfp is already disabled.</b>"))
+        else:
+            client = self.client
+            self.pfp_enabled = False
+            self.pfp_degree = 0
+            self.pfp_remove_latest = None
+
+            await message.edit(_("<b>Successfully disabled autobio, removing last profile pic.</b>"))
+            await client(functions.photos.DeletePhotosRequest(
+                await client.get_profile_photos(await client.get_me(), limit=1)
+            ))
+
     async def autobiocmd(self, message):
         """ Automatically changes your Telegram's bio with current time, usage:
-            .autobio <timeout, seconds> '<message, time as {time}>'"""
+            (prefix)autobio <timeout, seconds> '<message, time as {time}>'"""
 
         msg = utils.get_args(message)
         if len(msg) != 2:
@@ -52,7 +130,7 @@ class AutoProfileMod(loader.Module):
                 timeout_autobio = int(msg[0])
             except ValueError as e:
                 logger.warning(str(e))
-                return await message.edit(_("<b> Wrong time.</b>"))
+                return await message.edit(_("<b>Wrong time.</b>"))
         if '{time}' not in raw_bio:
             return await message.edit(_("<b>You haven't specified time position/Wrong format.</b>"))
 
@@ -82,7 +160,7 @@ class AutoProfileMod(loader.Module):
 
     async def autonamecmd(self, message):
         """ Automatically changes your Telegram name with current time, usage:
-            .autoname <timeout, seconds> '<message, time as {time}>'"""
+            (prefix)autoname <timeout, seconds> '<message, time as {time}>'"""
 
         msg = utils.get_args(message)
         if len(msg) != 2:
