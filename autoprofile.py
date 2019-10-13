191492fa8ea950d74d1d962bb76d931a05f501c2
pqhaa <usaguy1342@gmail.com>
2019-10-13 07:01:57 +0000

    autoprofile: Resolve most suggestions
    Signed-off-by: pqhaa <usaguy1342@gmail.com>

--- autoprofile.py
+++ autoprofile.py
@@ -46,7 +46,7 @@ class AutoProfileMod(loader.Module):
 
     async def autopfpcmd(self, message):
         """Rotates your profile picture every n seconds with x degrees, usage:
-           (prefix)autopfp <timeout> <degrees> <remove previous (last pfp)>
+           .autopfp <timeout> <degrees> <remove previous (last pfp)>
 
            Timeout - seconds
            Degrees - 60, -10, etc
@@ -54,35 +54,34 @@ class AutoProfileMod(loader.Module):
 
         msg = utils.get_args(message)
         if len(msg) != 3:
-            return await message.edit(_("<b>Autopfp requires three args. See the help for syntax.</b>"))
+            return await utils.answer(message, _("<b>Autopfp requires three args. See the help for syntax.</b>"))
 
         try:
             timeout_autopfp = int(msg[0])
         except ValueError as e:
             logger.warning(str(e))
-            return await message.edit(_("<b>Wrong time.</b>"))
+            return await utils.answer(message, _("<b>Wrong time.</b>"))
 
         try:
             degrees = int(msg[1])
         except ValueError as e:
             logger.warning(str(e))
-            return await message.edit(_("<b>Wrong degrees value.</b>"))
+            return await utils.answer(message, _("<b>Wrong degrees value.</b>"))
 
         if msg[2].lower() == 'true':
             delete_previous = True
         elif msg[2].lower() == 'false':
             delete_previous = False
         else:
-            return await message.edit(_("<b>Please pass true or false for previous pfp removing.</b>"))
+            return await utils.answer(message, _("<b>Please pass true or false for previous pfp removing.</b>"))
 
-        client = self.client
-        me = await client.get_me()
-        pfp = await client.download_profile_photo(me, file=bytes)
+        me = await self.client.get_me()
+        pfp = await self.client.download_profile_photo(me, file=bytes)
         raw_pfp = Image.open(BytesIO(pfp))
 
         self.pfp_remove_latest = delete_previous
         self.pfp_enabled = True
-        await message.edit("<b>Successfully enabled autopfp.</b>")
+        await utils.answer(message, "<b>Successfully enabled autopfp.</b>")
 
         while self.pfp_enabled:
             self.pfp_degree += degrees
@@ -92,12 +91,12 @@ class AutoProfileMod(loader.Module):
             bytes_img = buf.getvalue()
 
             if self.pfp_remove_latest:
-                await client(functions.photos.DeletePhotosRequest(
-                    await client.get_profile_photos(await client.get_me(), limit=1)
+                await self.client(functions.photos.DeletePhotosRequest(
+                    await self.client.get_profile_photos(await self.client.get_me(), limit=1)
                 ))
 
-            await client(functions.photos.UploadProfilePhotoRequest(
-                await client.upload_file(bytes_img)
+            await self.client(functions.photos.UploadProfilePhotoRequest(
+                await self.client.upload_file(bytes_img)
             ))
             await asyncio.sleep(timeout_autopfp)
 
@@ -105,38 +104,37 @@ class AutoProfileMod(loader.Module):
         """ Stop autobio cmd."""
 
         if self.pfp_enabled is False:
-            return await message.edit(_("<b>Autopfp is already disabled.</b>"))
+            return await utils.answer(message, _("<b>Autopfp is already disabled.</b>"))
         else:
-            client = self.client
             self.pfp_enabled = False
             self.pfp_degree = 0
             self.pfp_remove_latest = None
 
-            await message.edit(_("<b>Successfully disabled autobio, removing last profile pic.</b>"))
-            await client(functions.photos.DeletePhotosRequest(
-                await client.get_profile_photos(await client.get_me(), limit=1)
+            await self.client(functions.photos.DeletePhotosRequest(
+                await self.client.get_profile_photos(await self.client.get_me(), limit=1)
             ))
+            await utils.answer(message, _("<b>Successfully disabled autobio, removing last profile pic.</b>"))
 
     async def autobiocmd(self, message):
         """ Automatically changes your Telegram's bio with current time, usage:
-            (prefix)autobio <timeout, seconds> '<message, time as {time}>'"""
+            .autobio <timeout, seconds> '<message, time as {time}>'"""
 
         msg = utils.get_args(message)
         if len(msg) != 2:
-            return await message.edit(_("<b>AutoBio requires two args.</b>"))
+            return await utils.answer(message, _("<b>AutoBio requires two args.</b>"))
         else:
             raw_bio = msg[1]
             try:
                 timeout_autobio = int(msg[0])
             except ValueError as e:
                 logger.warning(str(e))
-                return await message.edit(_("<b>Wrong time.</b>"))
+                return await utils.answer(message, _("<b>Wrong time.</b>"))
         if '{time}' not in raw_bio:
-            return await message.edit(_("<b>You haven't specified time position/Wrong format.</b>"))
+            return await utils.answer(message, _("<b>You haven't specified time position/Wrong format.</b>"))
 
         self.bio_enabled = True
         self.raw_bio = raw_bio
-        await message.edit("<b>Successfully enabled autobio.</b>")
+        await utils.answer(message, "<b>Successfully enabled autobio.</b>")
 
         while self.bio_enabled is True:
             current_time = time.strftime("%H:%M")
@@ -150,34 +148,34 @@ class AutoProfileMod(loader.Module):
         """ Stop autobio cmd."""
 
         if self.bio_enabled is False:
-            return await message.edit(_("<b>Autobio is already disabled.</b>"))
+            return await utils.answer(message, _("<b>Autobio is already disabled.</b>"))
         else:
             self.bio_enabled = False
-            await message.edit(_("<b>Successfully disabled autobio, setting bio to without time.</b>"))
+            await utils.answer(message, _("<b>Successfully disabled autobio, setting bio to without time.</b>"))
             await self.client(functions.account.UpdateProfileRequest(
                 about=self.raw_bio.format(time="")
             ))
 
     async def autonamecmd(self, message):
         """ Automatically changes your Telegram name with current time, usage:
-            (prefix)autoname <timeout, seconds> '<message, time as {time}>'"""
+            .autoname <timeout, seconds> '<message, time as {time}>'"""
 
         msg = utils.get_args(message)
         if len(msg) != 2:
-            return await message.edit(_("<b>AutoName requires two args.</b>"))
+            return await utils.answer(message, _("<b>AutoName requires two args.</b>"))
         else:
             raw_name = msg[1]
             try:
                 timeout_autoname = int(msg[0])
             except ValueError as e:
                 logger.error(str(e))
-                return await message.edit(_("<b>Wrong time.</b>"))
+                return await utils.answer(message, _("<b>Wrong time.</b>"))
         if "{time}" not in raw_name:
-            return await message.edit(_("<b>You haven't specified time position/Wrong format.</b>"))
+            return await utils.answer(message, _("<b>You haven't specified time position/Wrong format.</b>"))
 
         self.name_enabled = True
         self.raw_name = raw_name
-        await message.edit(_("<b>Successfully enabled autoname.</b>"))
+        await utils.answer(message, _("<b>Successfully enabled autoname.</b>"))
 
         while self.name_enabled is True:
             current_time = time.strftime("%H:%M")
@@ -191,10 +189,10 @@ class AutoProfileMod(loader.Module):
         """ Stop autoname cmd."""
 
         if self.name_enabled is False:
-            return await message.edit(_("<b>Autoname is already disabled.</b>"))
+            return await utils.answer(message, _("<b>Autoname is already disabled.</b>"))
         else:
             self.name_enabled = False
-            await message.edit(_("<b>Successfully disabled autoname, setting name to without time.</b>"))
+            await utils.answer(message, _("<b>Successfully disabled autoname, setting name to without time.</b>"))
             await self.client(functions.account.UpdateProfileRequest(
                 first_name=self.raw_name.format(time="")
             ))
