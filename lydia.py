0b047ba4f0f822aa501aac83bb91c8b6b3e174cf
pqhaa <usaguy1342@gmail.com>
2019-10-16 20:23:45 +0100

    lydia: add cleaning commands
    Signed-off-by: Hackintosh 5 <hackintosh5@wildfyre.net>

--- lydia.py
+++ lydia.py
@@ -131,6 +131,16 @@ class LydiaMod(loader.Module):
             pass
         await message.edit(_("<code>AI disabled for this user.</code>"))
 
+    async def cleanlydiadisabledcmd(self, message):
+        """ Remove *all* lydia-disabled users from DB. """
+        self._db.set(__name__, 'allow', [])
+        return await utils.answer(message, _("<code>Successfully cleaned up lydia-disabled IDs</code>"))
+
+    async def cleanlydiasessionscmd(self, message):
+        """Remove *all* active and not active lydia sessions from DB"""
+        self._db.set(__name__, 'sessions', {})
+        return await utils.answer(message, _("<code>Successfully cleaned up lydia sessions.</code>"))
+
     async def watcher(self, message):
         if self.config["CLIENT_KEY"] == "":
             logger.debug("no key set for lydia, returning")
