f5821647b5d632628fb2a1c6af555f350b400f63
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-11-30 14:13:02 +0530

    admin_tools: upstream to new translation system (#31)
    * admin_tools: upstream to new translation system
    
    * admin_tools: remove redundant 'return'
    
    * admin_tools: fix HTML derps for strings.

--- admin_tools.py
+++ admin_tools.py
@@ -30,83 +30,92 @@ def register(cb):
     cb(BanMod())
 
 
+@loader.tds
 class BanMod(loader.Module):
     """Group administration tasks"""
+    strings = {"name": "Administration",
+               "ban_not_supergroup": "<b>I can't ban someone unless they're in a supergroup!</b>",
+               "unban_not_supergroup": "<b>I can't unban someone unless they're banned from a supergroup!</b>",
+               "kick_not_group": "<b>I can't kick someone unless they're in a group!</b>",
+               "ban_none": "<b>I can't ban no-one, can I?</b>",
+               "unban_none": "<b>I need someone to unbanned here.</b>",
+               "kick_none": "<b>I need someone to be kicked out of the chat.</b>",
+               "who": "<b>Who the hell is that?</b>",
+               "not_admin": "<b>Am I an admin here?</b>",
+               "banned": "<b>Banned</b> <code>{}</code> <b>from the chat!</b>",
+               "unbanned": "<b>Unbanned</b> <code>{}</code> <b>from the chat!</b>",
+               "kicked": "<b>Kicked</b> <code>{}</code> <b>from the chat!</b>"}
+
     def __init__(self):
-        self.name = _("Administration")
+        self.name = self.strings["name"]
 
     async def bancmd(self, message):
         """Ban the user from the group"""
         if not isinstance(message.to_id, PeerChannel):
-            return await utils.answer(message,
-                                      _("You can't ban someone unless they're in a supergroup! Try blocking them."))
+            return await utils.answer(message, self.strings["not_supergroup"])
         if message.is_reply:
             user = await utils.get_user(await message.get_reply_message())
         else:
             args = utils.get_args(message)
             if len(args) == 0:
-                return await utils.answer(message, _("I can't ban no-one, can I?"))
+                return await utils.answer(message, self.strings["ban_none"])
             user = await self.client.get_entity(args[0])
         if not user:
-            return await message.edit(_("Who the hell is that?"))
+            return await utils.answer(message, self.strings["who"])
         logger.debug(user)
         try:
             await self.client(EditBannedRequest(message.chat_id, user.id,
                                                 ChatBannedRights(until_date=None, view_messages=True)))
         except BadRequestError:
-            await message.edit(_("Am I an admin here?"))
+            await utils.answer(message, self.strings["not_admin"])
         else:
             await self.allmodules.log("ban", group=message.chat_id, affected_uids=[user.id])
-            await utils.answer(message, _("Banned <code>{}</code> from the chat!")
-                               .format(utils.escape_html(ascii(user.first_name))))
+            await utils.answer(message, self.strings["banned"].format(utils.escape_html(ascii(user.first_name))))
 
     async def unbancmd(self, message):
         """Lift the ban off the user."""
         if not isinstance(message.to_id, PeerChannel):
-            return await utils.answer(message, _("You can't unban someone unless they're banned from a supergroup!"))
+            return await utils.answer(message, self.strings["unban_not_supergroup"])
         if message.is_reply:
             user = await utils.get_user(await message.get_reply_message())
         else:
             args = utils.get_args(message)
             if len(args) == 0:
-                return await utils.answer(message, _("Who should be unbanned?"))
+                return await utils.answer(message, self.strings["unban_none"])
             user = await self.client.get_entity(args[0])
         if not user:
-            return await message.edit(_("I couldn't find who to unban."))
+            return await utils.answer(message, self.strings["who"])
         logger.debug(user)
         try:
             await self.client(EditBannedRequest(message.chat_id, user.id,
                               ChatBannedRights(until_date=None, view_messages=False)))
         except BadRequestError:
-            await message.edit(_("Am I an admin here?"))
+            await utils.answer(message, self.strings["not_admin"])
         else:
             await self.allmodules.log("unban", group=message.chat_id, affected_uids=[user.id])
-            await utils.answer(message, _("Unbanned <code>{}</code> from the chat!")
-                               .format(utils.escape_html(ascii(user.first_name))))
+            await utils.answer(message, self.strings["unbanned"].format(utils.escape_html(ascii(user.first_name))))
 
     async def kickcmd(self, message):
         """Kick the user out of the group"""
         if isinstance(message.to_id, PeerUser):
-            return await utils.answer(message,
-                                      _("You can't kick someone unless they're in a group! Try blocking them."))
+            return await utils.answer(message, self.strings["kick_not_group"])
         if message.is_reply:
             user = await utils.get_user(await message.get_reply_message())
         else:
             args = utils.get_args(message)
             if len(args) == 0:
-                return await utils.answer(message, _("Who should be kicked?"))
+                return await utils.answer(message, self.strings["kick_none"])
             user = await self.client.get_entity(args[0])
         if not user:
-            return await message.edit(_("I couldn't find who to kick."))
+            return await utils.answer(message, self.strings["who"])
         logger.debug(user)
         try:
             await self.client.kick_participant(message.chat_id, user.id)
         except BadRequestError:
-            await message.edit(_("Am I an admin here?"))
+            await utils.answer(message, self.strings["not_admin"])
         else:
             await self.allmodules.log("kick", group=message.chat_id, affected_uids=[user.id])
-            await message.edit(_("Kicked <code>{}</code> from the chat!")
-                               .format(utils.escape_html(ascii(user.first_name))))
+            await utils.answer(message, self.strings["kicked"].format(utils.escape_html(ascii(user.first_name))))
 
     async def client_ready(self, client, db):
         self.client = client
